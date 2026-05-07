044440ec53b2098f9779c9b04711b1d4435304a5
mattmarietta <mattmarietta24@gmail.com>
2026-05-07 10:36:47 -0700

    Added fixes for run build on vercel

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,15 +1,8 @@
 import { registerOTel } from "@vercel/otel"
-import {
-  ConsoleSpanExporter,
-  SimpleSpanProcessor,
-} from "@opentelemetry/sdk-trace-base"
 
 export function register() {
   registerOTel({
     serviceName: "miaomo.ai",
-    spanProcessors: [
-      "auto",
-      // new SimpleSpanProcessor(new ConsoleSpanExporter()),
-    ],
+    spanProcessors: ["auto"],
   })
 }
